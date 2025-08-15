62649363c385b4a0fa34d25989fc86a820daee7a
Salusfin <smitha.kamath2023@gmail.com>
2025-08-15 17:01:20 +0300

    react hook form with zod

---
+++ src/components/ReactHookForms.tsx
@@ -0,0 +1,93 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  fname: z.coerce
+    .string()
+    .regex(/^[A-Z]+[a-z]+$/)
+    .toUpperCase()
+    .min(3, { message: "Too short name should be atleast 3 charecters long" })
+    .max(30, { message: "Too Long name should be atmost 30 charecters long" }),
+  lname: z
+    .string()
+    .min(3, "Too short name should be atleast 3 charecters long")
+    .max(30, "Too Long name should be atmost 30 charecters long"),
+  email: z.email("should be an valid email address"),
+  tel: z
+    .number({
+      error: (issue) => {
+        if (issue.code === "invalid_type") {
+          return { message: `Value must be 8 digit phone number` };
+        }
+
+        //  defer to default
+        return undefined;
+      },
+    })
+    .min(100000000, {
+      message: "Mobile number shoudld be atlrast 8 digits long",
+    }),
+  title: z.string("Title is required"),
+  developer: z.boolean("Developer field is required"),
+});
+type FormDataProps = z.input<typeof schema>;
+export default function ReactHookForms() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    resolver: zodResolver(schema),
+  });
+  console.log(errors);
+  const onSubmit = (data: FormDataProps) => {
+    console.log(data);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="d-flex flex-column container gap-2 w-50 formHook"
+    >
+      <input
+        type="text"
+        placeholder="First name"
+        {...register("fname")}
+        className={errors.fname ? "error" : ""}
+      />
+      {errors.fname && <span>{errors.fname.message}</span>}
+
+      <input
+        type="text"
+        placeholder="Last name"
+        {...register("lname")}
+        className={errors.lname ? "error" : ""}
+      />
+      <input
+        type="text"
+        placeholder="Email"
+        {...register("email")}
+        className={errors.email ? "error" : ""}
+      />
+      <input
+        type="number"
+        placeholder="Mobile number"
+        {...register("tel", { valueAsNumber: true })}
+        className={errors.tel ? "error" : ""}
+      />
+      <select {...register("title")} className={errors.title ? "error" : ""}>
+        <option value="Mr">Mr</option>
+        <option value="Mrs">Mrs</option>
+        <option value="Miss">Miss</option>
+        <option value="Dr">Dr</option>
+      </select>
+
+      <input {...register("developer")} type="radio" value="Yes" />
+      <input {...register("developer")} type="radio" value="No" />
+
+      <input type="submit" />
+    </form>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  root: resolve(__dirname),
+  build: {
+    outDir: "../dist",
+  },
+
+  // Optional: Silence Sass deprecation warnings. See note below.
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          "import",
+          "mixed-decls",
+          "color-functions",
+          "global-builtin",
+        ],
+      },
+    },
+  },
+};
